c2af7d6cb1869983a984dc444efa7a8bc866aa23
App Academy Student <student@appacademy.io>
2013-09-24 17:21:00 -0700

    Add points_between to Board.

--- board.rb
+++ board.rb
@@ -4,6 +4,7 @@ require './rook.rb'
 require './knight.rb'
 require './bishop.rb'
 require './pawn.rb'
+require 'colorize'
 
 class Board
   attr_accessor :grid
@@ -19,11 +20,66 @@ class Board
     @grid[start_pos[0]][start_pos[1]] = nil
   end
 
+  def valid_move?(start_pos, end_pos)
+    piece = @grid[start_pos[0]][start_pos[1]]
+    unless piece.moves.include? (start_pos)
+      raise "That's not within the piece's move range!"
+    end
+
+
+  end
+
+  # def blocked?(piece, end_pos)
+  #   if piece.is_a?(Pawn)
+  #     @grid[end_pos[0][end_pos[1]]
+  #   elsif piece.is_a?(SlidingPiece)
+  #     return true if @grid[end_pos[0]][end_pos[1]].color == piece.color
+  #     x_change = end_pos[0] - piece.position[0]
+  #     y_change = end_pos[1] - piece.position[1]
+  #
+  #     x_values =
+  #   end
+  # end
+
+  def points_between(start_pos, end_pos)
+
+    x_change = end_pos[0] - start_pos[0] - 1
+    y_change = end_pos[1] - start_pos[1] - 1
+
+
+    y_range = (-(y_change.abs)..y_change.abs).to_a
+    if y_change < 0
+      y_range.select! { |y| y < 0 }.reverse!
+    else
+      y_range.select! { |y| y > 0 }
+    end
+
+    x_range = (-(x_change.abs)..x_change.abs).to_a
+    if x_change < 0
+      x_range.select! { |x| x < 0 }.reverse!
+    else
+      x_range.select! { |x| x > 0 }
+    end
+
+
+    points_between = []
+    [x_range.length, y_range.length].max.times do |index|
+      x_val = x_range[index] || 0
+      y_val = y_range[index] || 0
+
+      points_between << [x_val, y_val]
+    end
+
+    points_between
+  end
+
   def place_board
-    @grid[0] = back_row(0, :black)
-    @grid[1] = pawn_row(1, :black)
-    @grid[6] = pawn_row(6, :white)
-    @grid[7] = back_row(7, :white)
+    @grid[0] = back_row(0, "black")
+    @grid[1] = pawn_row(1, "black")
+    @grid[6] = pawn_row(6, "white")
+    @grid[7] = back_row(7, "white")
+
+    puts "Board Placed!".red
   end
 
   def back_row(row, color)
@@ -38,5 +94,20 @@ class Board
     end
   end
 
+  def show_board
+    print "   "
+    (1..8).each do |col_num|
+      print "|#{col_num}".rjust(4)
+    end
+    print "\n"
 
+    @grid.each_with_index do |row, index|
+      print "#{(index + 1)}|"
+      row.each do |piece|
+        print (piece ? "| #{piece.display} " : "|   ").ljust(3)
+      end
+      print "\n"
+
+    end
+  end
 end
--- piece.rb
+++ piece.rb
@@ -1,6 +1,9 @@
 class Piece
   attr_accessor :position, :color
 
+  DISPLAY_CODES = {:pawn => 9823, :rook => 9820, :knight => 9822,
+                  :bishop => 9821, :king => 9818, :queen => 9819}
+
   def initialize(position, color)
     @position = position
     @color = color
@@ -15,6 +18,22 @@ class Piece
 
   def on_board?(move)
     (1..7).include?(move[0]) && (1..7).include?(move[1])
+  end
+
+  def display
+    code = DISPLAY_CODES[:knight] if is_a?(Knight)
+    code = DISPLAY_CODES[:rook] if is_a?(Rook)
+    code = DISPLAY_CODES[:queen] if is_a?(Queen)
+    code = DISPLAY_CODES[:king] if is_a?(King)
+    code = DISPLAY_CODES[:bishop] if is_a?(Bishop)
+    code = DISPLAY_CODES[:pawn] if is_a?(Pawn)
+
+    # color == "white" ? [code].pack('U').white : [code].pack('U').blue
+    if color == "white"
+      [code].pack('U').white
+    else
+      [code].pack('U').blue
+    end
   end
 end
 
